d2fdaa2476f0efd0ed7f207279a8643d46c247d0
prosper <dikehprosper@gmail.com>
2023-06-12 12:20:13 +0100

    updated arg and execurabe path for puppeteer launch

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const puppeteer = require("puppeteer")
 const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 4000;
+require("dotenv").config();
 // app.use(cors());
 // if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
 //     chrome = require("chrome-aws-lambda");
@@ -25,11 +26,20 @@ app.get("/", async (req, res) => {
     // }
 
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        args: [
+            '--no-sandbox',
+            '--disabe-setuid-sandbox',
+            '--single-process',
+            '--no-zygote'
+        ],
+         ececutablePath: process.env.NODE_ENV === 'production' ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath()
+
+    });
 
     try {
         const page = await browser.newPage();
-      
+
         await page.goto("https://logigames.bet9ja.com/Games/Launcher?gameId=11000&provider=0&pff=1&skin=201");
 
         const html1 = await page.evaluate(() =>
