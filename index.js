f216ef7d880c84e5d9bd956b0dc38028c4ba5bde
prosper <dikehprosper@gmail.com>
2023-06-14 16:04:49 +0100

    updated

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 
 const puppeteer = require("puppeteer")
+const cors = require("cors");
 const express = require("express")
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 4000;
 require("dotenv").config();
 const fs = require("fs");
-const cors = require("cors");
+
 
 
 // Countdown section
